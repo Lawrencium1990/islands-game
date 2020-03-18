236f61002bcd898eea198a207354302e28738162
Philip Schölzel <philip.schoelzel@yahoo.de>
2020-03-18 14:49:04 +0100

    fix more syntax-errors

--- lib/islands_engine/game.ex
+++ lib/islands_engine/game.ex
@@ -1,7 +1,9 @@
 defmodule IslandsEngine.Game do
   use GenServer
 
-  alias IslandsEngine.{Board, Guesses, Rules}
+  alias IslandsEngine.{Board, Coordinate, Guesses, Island, Rules}
+
+  @players [:player1, :player2]
 
   def init(name) do
     player1 = %{name: name, board: Board.new(), guesses: Guesses.new()}
@@ -16,6 +18,10 @@ defmodule IslandsEngine.Game do
   def handle_info(:first, state) do
     IO.puts "I Bims vong Text her"
     {:noreply, state}
+  end
+
+  def demo_call(game) do
+    GenServer.call(game, :demo_call)
   end
 
   def handle_call(:demo_call, _from, state) do
@@ -34,20 +40,58 @@ defmodule IslandsEngine.Game do
     end
   end
 
-  def demo_call(game) do
-    GenServer.call(game, :demo_call)
+  def demo_cast(pid, new_value) do
+    GenServer.cast(pid, {:demo_cast, new_value})
   end
 
   def handle_cast({:demo_cast, new_value}, state) do
     {:no_reply, Map.put(state, :test, new_value)}
   end
 
-  def demo_cast(pid, new_value) do
-    GenServer.cast(pid, {:demo_cast, new_value})
-  end
-
   def start_link(name) when is_binary(name) do
     GenServer.start_link(__MODULE__, name, [])
+  end
+
+  def position_island(game, player, key, lon, lat) when player in @players do
+    GenServer.call(game, {:position_island, player, key, lon, lat})
+  end
+
+  defp update_board(state_data, player, board) do
+    Map.update!(state_data, player, fn player -> %{player | board: board} end)
+  end
+
+  def handle_call({:position_island, player, key, lon, lat}, _from, state_data) do
+    board = player_board(state_data, player)
+    # Lerne: with-condition nicht mit zeilenumbruch schreiben ->
+      # " with {:ok, rules} <- " anstatt
+      # " with
+      # {:ok, rules} <- "
+    with {:ok, rules} <-
+        Rules.check(state_data.rules, {:position_islands, player}),
+      {:ok, coordinate} <-
+        Coordinate.new(lon, lat),
+      {:ok, island} <-
+        Island.new(key, coordinate),
+      %{} = board <-
+        Board.position_island(board, key, island)
+    do
+      state_data
+      |> update_board(player, board)
+      |> update_rules(rules)
+      |> reply_success(:ok)
+    else
+      :error ->
+        {:reply, :error, state_data}
+      {:error, :invalid_coordinate} ->
+        {:reply, {:error, :invalid_coordinate}, state_data}
+      {:error, :invalid_island_type} ->
+        {:reply, {:error, :invalid_island_type}, state_data}
+    end
+  end
+
+  # Map.get(xx).yy --> genauere Funktion erfragen
+  defp player_board(state_data, player) do
+    Map.get(state_data, player).board
   end
 
   defp update_player2_name(state_data, name) do
@@ -57,7 +101,5 @@ defmodule IslandsEngine.Game do
   defp update_rules(state_data, rules), do: %{state_data | rules: rules}
 
   defp reply_success(state_data, reply), do: {:reply, reply, state_data}
-
-
 
 end
